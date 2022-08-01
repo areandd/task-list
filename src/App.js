0c0e68117141685424c5e53621b393aa4ddca947
Isaiah Desta <areandd@protonmail.com>
2022-08-01 16:29:38 +0000

    basic functionality for todo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import TodoList from './TodoList'
+import TodoList from './components/TodoList'
 import uuidv4 from 'uuid/v4'
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
